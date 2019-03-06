a4086fa8e26e7d4831fe8dbb20c3b22ef8529cb7
Tiago Garcia <tiago.tiago22@gmail.com>
2019-03-06 15:53:57 -0300

    add base request class

---
+++ lib/http_call/base.rb
@@ -0,0 +1,32 @@
+module HttCall do
+  class Base
+    attr_reader :url, :body, :header
+
+    def initialize(params)
+      @url     = params.fetch(:url)
+      @body    = params.fetch(:body, nil)
+      @header  = params.fetch(:header, {})
+      @attempt = 3
+    end
+
+    def call; end
+
+    private
+
+    attr_reader :attempt
+
+    def request
+      response = yield
+      raise 'Error' if res.is_a?(Net::HTTPSuccess)
+      response
+    rescue
+      retry_request
+    end
+
+    def retry_request
+      attempt -= 1
+      return response if attempt == 0
+      sleep(5)
+    end
+  end
+end
